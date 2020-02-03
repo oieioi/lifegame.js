292c472efe978c18a922963e5db39d6ac4765f0a
oieioi <atsuinatsu.samuifuyu@gmail.com>
2020-02-03 17:31:29 +0900

    Autoplay

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './Controller.css';
 
-function Controller({x, y, generation, onNextGeneration}) {
+function Controller({x, y, generation, nextGeneration, autoPlay}) {
   return (
     <div className="Controller">
-      <label>x: <input name="x" type="number" readOnly value={x} /></label>
-      <label>y:<input name="y" type="number" readOnly value={y} /></label>
+      <button name="start" onClick={() => autoPlay(true)}>start</button>
+      <button name="stop" onClick={() => autoPlay(false)}>stop</button>
+      <button name="next" onClick={nextGeneration}>next</button>
       <label>generation:<input name="generation" type="number" readOnly value={generation} /></label>
-      <button name="start">start</button>
-      <button name="stop">stop</button>
-      <button name="next" onClick={onNextGeneration}>next</button>
-      <button name="apply">apply</button>
     </div>
   );
 }
 
-export default Controller;
+export default React.memo(Controller);
--- src/components/Lifegame.js
+++ src/components/Lifegame.js
@@ -5,16 +5,15 @@ import logic from '../lib/LifegameLogic';
 
 function Lifegame({x, y}) {
   const initialCells = logic.twoDArray(x, y, false);
-
   const [cells, setCells] = React.useState(initialCells);
   const [generation, setGeneration] = React.useState(0);
+
   const nextGeneration = () => {
     const newCells = logic.nextCells(cells);
-
     setGeneration(generation + 1)
-
     setCells(newCells);
   }
+
   const setCell = (x, y, value) => {
     const newCells = cells.slice();
     const newLine = newCells[x].slice();
@@ -23,6 +22,17 @@ function Lifegame({x, y}) {
     setCells(newCells)
   }
 
+  const [autoPlayMode, setAutoPlayMode] = React.useState(false)
+
+  React.useEffect(()=>{
+    if (!autoPlayMode) return;
+
+    const timer = setInterval(() => {
+      nextGeneration();
+    }, 500)
+    return () => { clearInterval(timer) }
+  })
+
   const lines = cells.map((line, x) => {
     const cells = line.map((alive, y) => <Cell key={`${x},${y}`} x={x} y={y} alive={alive} onChange={setCell} />)
     cells.push(<br />)
@@ -30,7 +40,13 @@ function Lifegame({x, y}) {
   })
 
   return (<>
-      <Controller x={x} y={y} generation={generation} onNextGeneration={nextGeneration} />
+    <Controller
+      x={x}
+      y={y}
+      generation={generation}
+      nextGeneration={nextGeneration}
+      autoPlay={setAutoPlayMode}
+    />
       {lines}
     </>);
 }
