884aa36537a9ff477eb4c46231fa509420e84984
oieioi <atsuinatsu.samuifuyu@gmail.com>
2020-02-03 21:25:25 +0900

    initialize

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Lifegame from './Lifegame';
 
 function App() {
-  const [x, setX] = React.useState(20);
-  const [y, setY] = React.useState(20);
+  const [x, setX] = React.useState(50);
+  const [y, setY] = React.useState(50);
   const [destroy, setDestroy] = React.useState(false);
 
   React.useEffect(() => {
-    setTimeout(() => { setDestroy(false) }, 10)
+    if (destroy) {
+      setTimeout(() => { setDestroy(false) }, 10)
+    }
   })
 
   function resize() {
@@ -21,7 +23,7 @@ function App() {
         <a href="https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0" target="_blank" rel="noopener noreferrer" >コンウェイのライフゲーム</a>を実施します。
       </h1>
       <div>
-        <button onClick={resize}>resize</button>
+        <button onClick={resize}>初期化</button> 
         <label>x:<input name="x" onChange={(event) => setX(Number(event.target.value))} type="number" value={x} /></label>
         <label>y:<input name="y" onChange={(event) => setY(Number(event.target.value))} type="number" value={y} /></label>
       </div>
