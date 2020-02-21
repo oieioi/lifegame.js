7b6de837b36e28bc0530ab4962828c02502cc811
oieioi <atsuinatsu.samuifuyu@gmail.com>
2020-02-22 01:29:13 +0900

    Add github link

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,9 @@ function App() {
       <h1>
         <span role="img" aria-label="virus">🦠</span><a href="https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0" target="_blank" rel="noopener noreferrer">ライフゲーム</a><span role="img" aria-label="virus">🦠</span>
       </h1>
+      <a href="https://github.com/oieioi/lifegame.js" target="_blank" rel="noopener noreferrer" className="ribbon">
+        <img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_red_aa0000.png?resize=149%2C149" alt="Fork me on GitHub" data-recalc-dims="1" />
+      </a>
       <div>
         <button onClick={resize}>初期化</button> 
         <label>x:<input name="x" onChange={(event) => setX(Number(event.target.value))} type="number" value={x} /></label>
